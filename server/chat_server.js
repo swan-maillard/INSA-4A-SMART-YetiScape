df5f1fb518a5f4967e1f8c162f3464d4929a50da
elm-raoul <raoul.el-mir@insa-lyon.fr>
2024-04-29 15:07:01 +0200

    Chat implementation (partial 2)

--- server/chat_server.js
+++ server/chat_server.js
@@ -1,10 +1,28 @@
-import express from "express";
-import * as http from 'http';
+import { Server } from 'socket.io';
+import express from 'express';
+import { createServer } from 'http';
 
-const app = express();
-
-const http_r = http.Server(app);
+const app = express(); 
+const http_r = createServer();
+const io = new Server(http_r);
 const port = process.env.PORT || 3000;
+
+io.on("connection", function(socket) {
+
+    socket.on("user_join", function(data) {
+        this.username = data;
+        socket.broadcast.emit("user_join", data);
+    });
+
+    socket.on("chat_message", function(data) {
+        data.username = this.username;
+        socket.broadcast.emit("chat_message", data);
+    });
+
+    socket.on("disconnect", function(data) {
+        socket.broadcast.emit("user_leave", this.username);
+    });
+});
 
 app.get("/", function(req, res) {
     res.sendFile(process.cwd() + "/served_client_files/chat_client.html");
