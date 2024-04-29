433d6829d66f0e3a9dbdd615e06ba1c3994c75a9
lilia <liliakarouia@gmail.com>
2024-04-29 09:53:13 +0200

    revert init

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express();
-
-// exemple
-app.get("/api", (req,res)=>{
-    res.json({"users": ["user1", "user2"]})
-})
-
-app.listen(5000, console.log("Serveur connecté au port 5000"))
