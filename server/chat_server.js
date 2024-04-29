167222bbb5a7d45a9bce07e3a907eb31876c5cdf
elm-raoul <raoul.el-mir@insa-lyon.fr>
2024-04-29 17:25:06 +0200

    Implemted chat (+session)

--- server/chat_server.js
+++ server/chat_server.js
@@ -6,6 +6,7 @@ const app = express();
 const http = createServer(app);
 const io = new Server(http);
 
+const sessionsMap = {};
 
 app.get("/", function(req, res) {
     res.sendFile(process.cwd() + "/served_client_files/chat_client.html");
@@ -14,17 +15,35 @@ app.get("/", function(req, res) {
 io.on("connection", function(socket) {
 
     socket.on("user_join", function(data) {
-        this.username = data;
-        socket.broadcast.emit("user_join", data);
+        this.username = data.user;
+        sessionsMap[socket.id] = data.session_id
+        for (const [socketId, sessionId] of Object.entries(sessionsMap)) {
+            if (sessionId == data.session_id)
+            {
+                socket.broadcast.to(socketId).emit("user_join",  data.user);
+            }
+        }
     });
 
     socket.on("chat_message", function(data) {
         data.username = this.username;
-        socket.broadcast.emit("chat_message", data);
+        for (const [socketId, sessionId] of Object.entries(sessionsMap)) {
+            if (sessionId == data.session_id)
+            {
+                socket.broadcast.to(socketId).emit("chat_message",  data);
+            }
+        }
     });
 
     socket.on("disconnect", function(data) {
-        socket.broadcast.emit("user_leave", this.username);
+        const disconnectedSession = sessionsMap[socket.id]
+        delete sessionsMap[socket.id]        
+        for (const [socketId, sessionId] of Object.entries(sessionsMap)) {
+            if (sessionId == disconnectedSession)
+            {
+                socket.broadcast.to(socketId).emit("user_leave", this.username);
+            }
+        }
     });
 });
 
