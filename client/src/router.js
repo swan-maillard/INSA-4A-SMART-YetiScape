a1fd780d24c5cce5908d380eb37e5db60e0af760
Ambreeeeee <ambre.hutier@gmail.com>
2024-04-30 11:07:51 +0200

    Modification router

--- client/src/router.js
+++ client/src/router.js
@@ -1,9 +1,11 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
-import HomeView from './views/WaitingView.vue'
+import HomeView from './views/HomeView.vue'
+import WaitingView from './views/WaitingView.vue'
 
 const routes = [
   { path: '/', component: HomeView },
+  { path: '/waiting', component: WaitingView },
 ]
 
 const router = createRouter({
