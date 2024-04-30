d255d5371fb9b35c83bff63a57c6bc1a597df07c
Ambreeeeee <ambre.hutier@gmail.com>
2024-04-30 10:32:38 +0200

    Ajout page attente sans css

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,9 +5,12 @@
 </script>
 
 <template>
-  <main>
-    <RouterView></RouterView>
-  </main>
+  <head></head>
+  <body>
+    <main class="background-init">
+      <RouterView></RouterView>
+    </main>
+  </body>
 </template>
 
 
@@ -16,8 +19,39 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  height: 100%;
+}
+
+body, html{
+  height: 100%;
+  margin: 0;
+}
+.background-init{
+    background-image: url("./assets/background_init.jpg");
+
+    /* Full height */
+    height: 100%;
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size:cover;
+}
+
+.flex
+{
+  display: flex;
+  flex-direction: row;
+  width:100%;
+}
+
+.justify-between{
+  justify-content: space-between;
+}
+
+.m-1{
+  margin-left: 1rem;
+  margin-right: 1rem;
 }
 </style>
---
+++ client/src/views/WaitingView.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="m-1">
+    <div class="flex justify-between">
+        <p class="m-1">id de la partie : {{ idGame }}</p>
+        <p class="m-1">{{ idGamer }}/3</p>
+    </div>
+    <div class="flex">
+        <div class="card width-100" v-for="gamer in gamers" :key="gamer.id">
+            <div class="">
+            <h3 class="">{{gamer.name}}</h3>
+            </div>
+        </div>
+    </div>
+    <button v-if="idGamer == 3">Lancer la partie</button>
+</div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let idGame = 0;
+let idGamer = 0;
+
+const gamers = ref([
+    {id: idGamer++, name:'Jean'},
+    {id: idGamer++, name:'Marc'},
+    {id: idGamer++, name:'Petit'}
+]);
+</script>
+
+<style>
+.card{
+    border-radius: 3%;
+
+}
+
+.width-100{
+    width: 100%;
+    justify-content: center;
+    text-align: center
+}
+
+.testimonial-card .card-up {
+height: 120px;
+overflow: hidden;
+border-top-left-radius: 0.25rem;
+border-top-right-radius: 0.25rem;
+}
+
+.testimonial-card .avatar {
+width: 110px;
+margin-top: -60px;
+overflow: hidden;
+border: 3px solid #fff;
+border-radius: 50%;
+}
+</style>
