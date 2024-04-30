aa4b509668a44477f691dbeeb28151e64685fd91
Ambreeeeee <ambre.hutier@gmail.com>
2024-04-30 14:34:21 +0200

    Ajout Bootstrap cote client

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import router from './router.js'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App)
   .use(router)
