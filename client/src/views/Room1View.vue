6e97a9c1be6b3127664563a3d8a72faedf49ff1b
Alex4969 <alex.biaud@gmail.com>
2024-05-02 09:07:45 +0200

    add room page

---
+++ client/src/views/Room1View.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+
+
+<template>
+    <body id="allScreen">
+        <div id="GameScreen" class="d-flex flex-raw">
+            <div id="inventaire">
+            </div>
+            <div id="jeu">
+            </div>
+        </div>
+    </body>
+</template>
+
+
+
+<style scoped>
+    #GameScreen {
+        width: 1280px;
+        height: 720px;
+    }
+    #inventaire {
+        width:10%;
+        height:100%;
+    }
+    #jeu {
+        width:90%;
+        height:100%;
+    }
+    div {
+        border: 2px solid red;
+    }
+</style>
