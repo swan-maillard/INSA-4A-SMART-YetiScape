b4590ea1f325af189c4f8ad7a9e8d13aa56edac2
Swan <maillard.swan@gmail.com>
2024-05-02 11:24:34 +0200

    Server socket

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,13 +5,20 @@ import gamesRoutes from './routes/gamesRoutes';
 import cors from 'cors';
 import gameRoutes from './routes/gameRoutes';
 import { checkAuthAccessGame } from './JWT';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 
 // Boot express
 const app: Application = express();
 
+const http = createServer(app);
+const io = new Server(http);
+
+const sessionsMap: { [key: string]: string } = {};
+
 // Specify allowed origins
 const corsOptions = {
-  origin: ['http://localhost:8080', 'http://localhost:8081', 'http://localhost:8082'],  // Add other origins as needed
+  origin: ['http://localhost:8080', 'http://localhost:8081', 'http://localhost:8082'], // Add other origins as needed
 };
 
 // Use the CORS middleware with options
@@ -27,4 +34,38 @@ app.use('/users', usersRoutes);
 app.use('/games', gamesRoutes);
 app.use('/game', checkAuthAccessGame, gameRoutes);
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+interface CustomSocket extends Socket {
+  username?: string;
+}
+io.on('connection', function (socket: CustomSocket) {
+  socket.on('user_join', function (data: { [key: string]: unknown }) {
+    socket.username = data.user as string;
+    sessionsMap[socket.id] = data.session_id as string;
+    for (const [socketId, sessionId] of Object.entries(sessionsMap)) {
+      if (sessionId == data.session_id) {
+        socket.broadcast.to(socketId).emit('user_join', data.user);
+      }
+    }
+  });
+
+  socket.on('chat_message', function (data: { [key: string]: unknown }) {
+    data.username = socket.username;
+    for (const [socketId, sessionId] of Object.entries(sessionsMap)) {
+      if (sessionId == data.session_id) {
+        socket.broadcast.to(socketId).emit('chat_message', data);
+      }
+    }
+  });
+
+  socket.on('disconnect', function () {
+    const disconnectedSession = sessionsMap[socket.id];
+    delete sessionsMap[socket.id];
+    for (const [socketId, sessionId] of Object.entries(sessionsMap)) {
+      if (sessionId == disconnectedSession) {
+        socket.broadcast.to(socketId).emit('user_leave', socket.username);
+      }
+    }
+  });
+});
+
+http.listen(port, () => console.log(`Server is listening on port ${port}`));
