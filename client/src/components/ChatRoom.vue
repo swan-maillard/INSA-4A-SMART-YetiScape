b8e68af7ea2140c8e8251a6ee739cd9c4d085bb8
elm-raoul <raoul.el-mir@insa-lyon.fr>
2024-05-06 11:11:34 +0200

    Added client voice chat support
    Co-authored-by: lilia <liliakarouia@gmail.com>

--- client/src/components/ChatRoom.vue
+++ client/src/components/ChatRoom.vue
@@ -3,11 +3,174 @@ import { ref } from "vue";
 import socketio from "@/services/socketio";
 import useAuth from "@/stores/auth.store";
 
+
 const auth = useAuth();
+let callId = auth.game.callId
 
 const messages = ref([]);
 const newMessage = ref("");
 const collapsed = ref(false);
+
+//Voice setup
+navigator.getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia ||
+  navigator.msGetUserMedia;
+
+// State
+var me = {};
+var myStream;
+var peers = {};
+
+initVoiceChat()
+
+function initVoiceChat() {
+  init();
+
+  // Start everything up
+  function init() {
+    if (!navigator.getUserMedia) return unsupported();
+
+    getLocalAudioStream(function (err, stream) {
+      if (err || !stream) return;
+
+      connectToPeerJS(function (err) {
+        if (err) return;
+
+        registerIdWithServer(me.id);
+        // eslint-disable-next-line
+        $.get("https://" + window.location.hostname + ":3000/chat/" + callId + ".json").then((call) => {
+          if (call.peers.length) callPeers();
+        });
+      });
+    });
+  }
+
+  // Connect to PeerJS and get an ID
+  function connectToPeerJS(cb) {
+    display("Connecting to PeerJS...");
+    // eslint-disable-next-line
+    me = new Peer({ host: "/", secure: true, port: 3000, path: "/peer" });
+
+    me.on("call", handleIncomingCall); // Quand on recoit un call
+
+    me.on("open", function () {
+      display("Connected.");
+      display("ID: " + me.id);
+      cb && cb(null, me);
+    });
+
+    me.on("error", function (err) {
+      display(err);
+      cb && cb(err);
+    });
+  }
+
+  // Add our ID to the list of PeerJS IDs for this call
+  function registerIdWithServer() {
+    display("Registering ID with server...");
+    // eslint-disable-next-line
+    $.post("https://" + window.location.hostname + ":3000/chat/" + callId + "/addpeer/" + me.id);
+  }
+
+  /*
+  // Remove our ID from the call's list of IDs
+  function unregisterIdWithServer() {
+    // eslint-disable-next-line
+    $.post("/" + callId + "/removepeer/" + me.id);
+  }
+  */
+
+  // Call each of the peer IDs using PeerJS
+  function callPeers() {
+    // Permet de call TOUS les peers du call
+    // eslint-disable-next-line
+    $.get("https://" + window.location.hostname + ":3000/chat/" + callId + ".json").then((call) => {
+      call.peers.forEach(callPeer);
+    });
+  }
+
+  function callPeer(peerId) {
+    // Petmet de call UN peer du call
+    display("Calling " + peerId + "...");
+    var peer = getPeer(peerId);
+    peer.outgoing = me.call(peerId, myStream);
+
+    peer.outgoing.on("error", function (err) {
+      display(err);
+    });
+
+    peer.outgoing.on("stream", function (stream) {
+      display("Connected to " + peerId + ".");
+      addIncomingStream(peer, stream);
+    });
+  }
+
+  // When someone initiates a call via PeerJS
+  function handleIncomingCall(incoming) {
+    display("Answering incoming call from " + incoming.peer);
+    var peer = getPeer(incoming.peer);
+    peer.incoming = incoming;
+    incoming.answer(myStream);
+    peer.incoming.on("stream", function (stream) {
+      addIncomingStream(peer, stream);
+    });
+  }
+
+  // Add the new audio stream. Either from an incoming call, or
+  // from the response to one of our outgoing calls
+  function addIncomingStream(peer, stream) {
+    display("Adding incoming stream from " + peer.id);
+    peer.incomingStream = stream;
+    playStream(stream);
+  }
+
+  // Create an <audio> element to play the audio stream
+  function playStream(stream) {
+    // eslint-disable-next-line
+    var audio = $("<audio autoplay />").appendTo("body");
+    audio[0].srcObject = stream;
+  }
+
+  // Get access to the microphone
+  function getLocalAudioStream(cb) {
+    display('Trying to access your microphone. Please click "Allow".');
+
+    navigator.getUserMedia(
+      { video: false, audio: true },
+
+      function success(audioStream) {
+        display("Microphone is open.");
+        myStream = audioStream;
+        if (cb) cb(null, myStream);
+      },
+
+      function error(err) {
+        display(
+          "Couldn't connect to microphone. Reload the page to try again."
+        );
+        if (cb) cb(err);
+      }
+    );
+  }
+
+  ////////////////////////////////////
+  // Helper functions
+  function getPeer(peerId) {
+    return peers[peerId] || (peers[peerId] = { id: peerId });
+  }
+
+  function unsupported() {
+    display("Your browser doesn't support getUserMedia.");
+  }
+
+  function display(message) {
+    console.log(message);
+  }
+}
+
+//End of voice setup
 
 const socket = socketio.socket;
 
